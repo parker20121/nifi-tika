ddf084bdcc134c3dc54488f71be7882b7da3a67f
Matt Parker <parker20121@gmail.com>
2016-07-25 16:07:52 -0400

    Reworked project structure to match NIFI NAR components

--- nifi-tika-processors/src/main/java/com/giantoak/nifi/processors/tika/TikaProcessor.java
+++ nifi-tika-processors/src/main/java/com/giantoak/nifi/processors/tika/TikaProcessor.java
@@ -1,4 +1,4 @@
-package com.giantoak.nifi.processor;
+package com.giantoak.nifi.processors.tika;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
